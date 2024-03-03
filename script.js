9c0c0c7b29d5c62f03b1a80ef1f50b3106e89482
vincent-ckxzvb <81626638+vincent-ckxzvb@users.noreply.github.com>
2024-03-03 15:48:07 +0800

    Update and rename script.js to Blank

--- script.js
+++
@@ -1,8 +0,0 @@
-function showMessage() {
-    const message = document.getElementById('message');
-    message.style.opacity = 1;
-    setTimeout(() => {
-        message.style.opacity = 0;
-    }, 2000);
-}
-
